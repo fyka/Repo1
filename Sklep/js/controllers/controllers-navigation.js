182afb041053b9655abd95f2be0b8147c5767b54
fyka <frankiefy19@gmail.com>
2016-11-27 19:50:57 +0100

    factory i storage 27

---
+++ Sklep/js/controllers/controllers-navigation.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var controllersNavigation = angular.module( 'controllersNavigation' , [] );
+
+
+controllersNavigation.controller( 'navigation' , [ '$scope' , '$location' , 'store' , function( $scope , $location, store ){
+
+	$scope.navigation = function () {
+		if ( /^\/admin/.test( $location.path() ) )
+			return 'partials/admin/navigation.html';
+		else
+			return 'partials/site/navigation.html';
+	};
+
+
+	$scope.isActive = function ( path ) {
+		return $location.path() === path;
+	};
+
+
+    store.set('test', 'dziala!')
+
+}]);
+
+
